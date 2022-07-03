e4441aeb25c5bafb0ad393d8ba4fd192845fa9d6
*Diogo <diogolimakas@hotmail.com>
2022-07-03 14:10:29 -0300

    add formulario/orcamento

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 import menuMobile from "./modules/menu-mobile.js";
-import commonQuestions from "./modules/common-questions.js"
-
+import commonQuestions from "./modules/common-questions.js";
+import forms from "./modules/forms.js";
+import url from "./modules/url.js";
 
 menuMobile();
 commonQuestions();
+forms()
+url()
